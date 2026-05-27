2f7cf7762a944a6ac8738a51bdc5df6edc331157
Marizet Henriques <marizet.contact@gmail.com>
2026-05-27 14:45:04 +0100

    selecionar elementos

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+const mensagem = document.querySelector('.message');
+const pontuacao = document.querySelector('.score');
+const maximo = document.querySelector('.highscore');
+const numero = document.querySelector('.number');
+const input = document.querySelector('.guess');
+const botaoVerificar = document.querySelector('.check');
+const botaoReiniciar = document.querySelector('.again');
